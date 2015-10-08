f2caf61a34136df606ee1177dbc904a7b6850a47
nickrobertson <nick.robertson@octanner.com>
2015-10-08 16:51:37 -0600

    finished ex3

---
+++ ex3.rb
@@ -0,0 +1,48 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}" # addition  Division: Division is first = 30
+puts "Roosters #{100 - 25 * 3 % 4}" # minus times modululo: times is first = 3
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # % then / then left to right = 7
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 # 5 < -2 = false
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #true
+puts "Is it greater or equal? #{5 >= -2}" #true
+puts "Is it less or equal? #{5 <= -2}" #false
+
+#Study Drills
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0 + 30.0 / 6.0}" # addition  Division: Division is first = 30
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}" # minus times modululo: times is first = 3
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # % then / then left to right = 7
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0 # 5 < -2 = false
+
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5.0 > -2.0}" #true
+puts "Is it greater or equal? #{5.0 >= -2.0}" #true
+puts "Is it less or equal? #{5.0 <= -2.0}" #false
