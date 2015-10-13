8f3fc0bc2cee0ea3883910a865f649fb467bd69a
nickrobertson <nick.robertson@octanner.com>
2015-10-13 09:35:19 -0600

    ex13 is finished

---
+++ ex13.rb
@@ -0,0 +1,13 @@
+first, second, third = ARGV
+
+
+puts "Your first variable is: #{first}."
+puts "Your second variable is: #{second}."
+puts "Your third variable is: #{third}."
+
+
+
+print "How many variables are you passing in?"
+num = $stdin.gets.chomp
+
+print "You have #{num} variables."
